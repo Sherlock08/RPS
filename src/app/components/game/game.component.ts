417179f62be5a164d20f85e7ed7b604858f2ae6e
Sherlock08 <anandverma552@gmail.com>
2020-06-11 01:55:40 +0530

    Code updated

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -5,21 +5,71 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent implements OnInit {
+export class GameComponent {
   myScore: number = 0;
-  computerScore : number = 0;
-  computerOptions: Array<string>=['rock','paper','scissors'];
+  computerScore: number = 0;
+  myChoice: string;
+  computerChoice: string;
+  computerOptions: Array<string> = ['rock', 'paper', 'scissors'];
   computerNumber: number;
+  resultLabel: string = "Let's Play!!"
+  playerChoiceImage: string = "rock";
+  computerChoiceImage: string = "rock";
 
   constructor() { }
 
-  ngOnInit() {
-  }
-  
-  startGame() {
-    this.computerNumber = Math.floor(Math.random()*3);
-    console.log(this.computerOptions[this.computerNumber]);
+  buttonClicked(buttonName) {
+    this.playerChoiceImage = "rock";
+    this.computerChoiceImage = "rock";
+    this.computerNumber = Math.floor(Math.random() * 3);
+    this.myChoice = buttonName;
+    Array.from(document.getElementsByClassName('player-image') as HTMLCollectionOf<HTMLElement>)[0].style.animation = "shakeUser 2s ease";
+    Array.from(document.getElementsByClassName('computer-image') as HTMLCollectionOf<HTMLElement>)[0].style.animation = "shakeComputer 2s ease";
+    setTimeout(() => {
+      this.playerChoiceImage = buttonName;
+      this.computerChoice = this.computerOptions[this.computerNumber];
+      this.computerChoiceImage = this.computerChoice;
+      this.compareHands(this.myChoice, this.computerChoice)
+      Array.from(document.getElementsByClassName('player-image') as HTMLCollectionOf<HTMLElement>)[0].style.animation = "";
+      Array.from(document.getElementsByClassName('computer-image') as HTMLCollectionOf<HTMLElement>)[0].style.animation = "";
+     
+    }, 2000);
     
-  }
 
+  }
+  compareHands(myChoice, computerChoice) {
+   
+    if (myChoice === computerChoice) {
+      this.resultLabel = "It's a Tiee!!"
+    }
+    if (myChoice === 'rock') {
+      if (computerChoice === 'scissors') {
+        this.resultLabel = "You win!! Yay!!"
+        this.myScore++;
+      } else if (computerChoice === 'paper') {
+        this.resultLabel = "You lose!! Sed Lyf!!"
+        this.computerScore++;
+      }
+    }
+    if (myChoice === 'paper') {
+
+      if (computerChoice === 'rock') {
+        this.resultLabel = "You win!! Yay!!"
+        this.myScore++;
+      } else if (computerChoice === 'scissors') {
+        this.resultLabel = "You lose!! Sed Lyf!!"
+        this.computerScore++;
+      }
+    }
+    if (myChoice === 'scissors') {
+      if (computerChoice === 'paper') {
+        this.resultLabel = "You win!! Yay!!"
+        this.myScore++;
+      } else if (computerChoice === 'rock') {
+        this.resultLabel = "You lose!! Sed Lyf!!"
+        this.computerScore++;
+      }
+    }
+  }
 }
+
